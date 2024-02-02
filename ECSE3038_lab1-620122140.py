6ee716b45adbf2657b54bb6b3018fcc930fa4e56
JADA242 <158534853+JADA242@users.noreply.github.com>
2024-02-01 21:48:49 -0500

    Lab 1
    The voltage drop between each resistor in a potential divider circuit is computed using this function. It formats the voltage drops to three decimal places followed by "v" after dividing the entire voltage supply proportionately across the resistors depending on their resistance levels.

--- ECSE3038_lab1-620122140.py
+++ ECSE3038_lab1-620122140.py
@@ -1,7 +1,26 @@
+#Q1
 def parallel(resistors):
+
+
     tot_inverse = sum(1 / resistor for resistor in resistors)
     effect_resistance = 1 / tot_inverse
     return f"{effect_resistance:.3f} ohms"
 
 
 print(parallel([440, 1030, 5600]))
+
+#Q2
+
+def pot_divider(voltage_supply, resistors):
+
+
+
+    tot_resistance = sum(resistors)
+    voltage_drops = [voltage_supply * (resistor / tot_resistance) for resistor in resistors]
+    formatfor_voltages = [f"{voltage:.3f}v" for voltage in voltage_drops]
+    return formatfor_voltages
+
+
+voltages = pot_divider(9, [3000, 1000])
+for voltage in voltages:
+    print(voltage)
